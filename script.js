ef61908f07e9023343dcf04d7ea94aa2223cbf73
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-14 18:04:05 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// const resumeLists = document.querySelectorAll('.resume-list')
+const resumeLists = document.querySelectorAll('.resume-list')
 
-// // navbar actions and all section actions along with cube rotation when navbar is clicked
-
-
-// // resume section when clicking tab-list
-// resumeLists.forEach((list, idx) => {
-//     list.addEventListener('click', () => {
-//         document.querySelector('.resume-list.active').classList.remove('active')
-//         list.classList.add('active')
-//     })
-// })
-
-// // portfolio section when clicking tab-list
+// navbar actions and all section actions along with cube rotation when navbar is clicked
 
 
-// // visibility for contact section when reloading (cube reloading animation)
+// resume section when clicking tab-list
+resumeLists.forEach((list, idx) => {
+    list.addEventListener('click', () => {
+        document.querySelector('.resume-list.active').classList.remove('active')
+        list.classList.add('active')
+    })
+})
+
+// portfolio section when clicking tab-list
+
+
+// visibility for contact section when reloading (cube reloading animation)
