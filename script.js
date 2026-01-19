e21e563e9b516434e6e62c6acd5f62e47ea7825b
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-19 19:54:40 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const resumeLists = document.querySelectorAll('.resume-list')
 const resumeBoxs = document.querySelectorAll('.resume-box')
 const portfolioLists = document.querySelectorAll('.portfolio-list')
+const portfolioBoxs = document.querySelectorAll('.portfolio-box')
 
 
 // navbar actions and all section actions along with cube rotation when navbar is clicked
@@ -23,6 +24,9 @@ portfolioLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
         document.querySelector('.portfolio-list.active').classList.remove('active')
         list.classList.add('active')
+
+        document.querySelector('.portfolio-box.active').classList.remove('active')
+        portfolioBoxs[idx].classList.add('active')
     })
 
 })
