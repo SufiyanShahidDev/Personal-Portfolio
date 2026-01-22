91ca1bc236597d1c94056786a9fd57c1d088a0a0
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-22 21:51:33 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const navs = document.querySelectorAll('.nav-list li')
 
-
 const resumeLists = document.querySelectorAll('.resume-list')
 const resumeBoxs = document.querySelectorAll('.resume-box')
 const portfolioLists = document.querySelectorAll('.portfolio-list')
@@ -15,7 +14,7 @@ resumeLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
         document.querySelector('.resume-list.active').classList.remove('active')
         list.classList.add('active')
-        
+
         document.querySelector('.resume-box.active').classList.remove('active')
         resumeBoxs[idx].classList.add('active')
     })
