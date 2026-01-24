eb369cf3394a6c94ce39f9370d8b6efde9d302a0
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-01-24 21:36:27 +0500

    Initial commit

--- script.js
+++ script.js
@@ -48,11 +48,11 @@ resumeLists.forEach((list, idx) => {
 
 portfolioLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
-        document.querySelector('.portfolio-list.active').classList.remove('active')
-        list.classList.add('active')
+        document.querySelector('.portfolio-list.active').classList.remove('active');
+        list.classList.add('active');
 
-        document.querySelector('.portfolio-box.active').classList.remove('active')
-        portfolioBoxs[idx].classList.add('active')
+        document.querySelector('.portfolio-box.active').classList.remove('active');
+        portfolioBoxs[idx].classList.add('active');
     })
 
 })
